bb8db022f7def9b60744c0455dde0868711421cb
edde746 <86283021+edde746@users.noreply.github.com>
2023-05-12 06:01:10 +0200

    remove pagination for an uninterrupted flow

--- src/routes/r/[subreddit]/+page.svelte
+++ src/routes/r/[subreddit]/+page.svelte
@@ -7,6 +7,7 @@
   import Header from '$lib/Header.svelte';
   import he from 'he';
   import Spinner from '$lib/Spinner.svelte';
+  import Masonry from '@edde746/svelte-masonry';
 
   export let data: PageData;
 
@@ -26,8 +27,14 @@
     nsfwEnabled = localStorage.getItem('nsfw') === 'true';
   });
 
+  const filterPost = (post: any) => {
+    if (!post.preview?.images?.at(0)?.source || !post.url) return false;
+    if (errored.includes(post.id)) return false;
+    return true;
+  };
+
   $: postCount = data.children.flat().reduce((acc, cur) => {
-    return cur.preview?.images[0]?.source && cur.url && !errored.includes(cur.id) ? acc + 1 : acc;
+    return filterPost(cur) ? acc + 1 : acc;
   }, 0);
 
   const findBestFittingImageUrl = (
@@ -53,7 +60,7 @@
 <svelte:window
   on:scroll={() => {
     if (loading || outOfPosts || postCount == 0) return;
-    if (window.innerHeight * 1.5 + window.scrollY >= document.body.offsetHeight) {
+    if (window.innerHeight * 1.6 + window.scrollY >= document.body.offsetHeight) {
       loading = true;
 
       const params = Object.fromEntries(data.params);
@@ -89,79 +96,64 @@
     <Spinner />
   </div>
 {:else}
-  {#each data.children as page, i}
-    <div class="md:columns-2 lg:columns-3 gap-4">
-      {#each page as post}
-        {@const firstImage = post.preview?.images[0]?.source}
-        {@const isPostInView = postsInView.includes(post.id)}
-        <div
-          class="media-container"
-          use:inview={inviewOptions}
-          on:inview_change={(event) => {
-            if (event.detail.inView) {
-              postsInView = [...postsInView, post.id];
-            } else {
-              postsInView = postsInView.filter((p) => p !== post.id);
+  <Masonry update={postCount} rearrange>
+    {#each data.children.flat().filter(filterPost) as post}
+      {@const firstImage = post.preview?.images[0]?.source}
+      {@const isPostInView = postsInView.includes(post.id)}
+      <div
+        class="media-container"
+        use:inview={inviewOptions}
+        on:inview_change={(event) => {
+          if (event.detail.inView) {
+            postsInView = [...postsInView, post.id];
+          } else {
+            postsInView = postsInView.filter((p) => p !== post.id);
+          }
+        }}
+      >
+        <a
+          class="w-full relative block"
+          style="aspect-ratio: {firstImage.width}/{firstImage.height}"
+          href={post.url}
+          on:click={(e) => {
+            if (post.nsfw && !nsfwEnabled) {
+              nsfwEnabled = confirm('Do you wish to enable NSFW content?');
+              localStorage.setItem('nsfw', String(nsfwEnabled));
+              e.preventDefault();
             }
           }}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          {#if firstImage && post.url && !errored.includes(post.id)}
-            <a
-              class="w-full mb-4 relative block"
-              style="aspect-ratio: {firstImage.width}/{firstImage.height}"
-              href={post.url}
-              on:click={(e) => {
-                if (post.nsfw && !nsfwEnabled) {
-                  nsfwEnabled = confirm('Do you wish to enable NSFW content?');
-                  localStorage.setItem('nsfw', String(nsfwEnabled));
-                  e.preventDefault();
-                }
-              }}
-              target="_blank"
-              rel="noopener noreferrer"
+          {#if post.nsfw && !nsfwEnabled}
+            <div
+              class="absolute inset-0 backdrop-blur-lg z-20 flex items-center justify-center flex-col text-lg font-bold"
             >
-              {#if post.nsfw && !nsfwEnabled}
-                <div
-                  class="absolute inset-0 backdrop-blur-lg z-20 flex items-center justify-center flex-col text-lg font-bold"
-                >
-                  NSFW
-                </div>
-              {/if}
-              <div class="absolute inset-0 bg-neutral-200 dark:bg-neutral-800" />
-              {#if isPostInView}
-                {#if post.media?.reddit_video}
-                  <video src={post.media.reddit_video.fallback_url} class="media" autoplay loop muted />
-                {:else if post.preview?.reddit_video_preview}
-                  <video src={post.preview.reddit_video_preview.fallback_url} class="media" autoplay loop muted />
-                {:else if post.preview?.images[0]}
-                  <img
-                    src={findBestFittingImageUrl(
-                      post.preview.images[0].resolutions,
-                      document.querySelector('.media-container').clientWidth
-                    )}
-                    alt={post.title}
-                    class="media"
-                    on:error={() => {
-                      console.log('error', post.url);
-                      errored = [...errored, post.id];
-                    }}
-                  />
-                {/if}
-              {/if}
-            </a>
+              NSFW
+            </div>
           {/if}
-        </div>
-      {/each}
-    </div>
-
-    {#if i != data.children.length - 1}
-      <div class="my-5 flex items-center gap-4">
-        <hr class="w-full" />
-        <div class="text-center shrink-0">Page {i + 1}</div>
-        <hr class="w-full" />
+          <div class="absolute inset-0 bg-neutral-200 dark:bg-neutral-800" />
+          {#if isPostInView}
+            {#if post.media?.reddit_video}
+              <video src={post.media.reddit_video.fallback_url} class="media" autoplay loop muted />
+            {:else if post.preview?.reddit_video_preview}
+              <video src={post.preview.reddit_video_preview.fallback_url} class="media" autoplay loop muted />
+            {:else if post.preview?.images[0]}
+              <img
+                src={findBestFittingImageUrl(
+                  post.preview.images[0].resolutions,
+                  document.querySelector('.media-container')?.clientWidth || 300
+                )}
+                alt={post.title}
+                class="media"
+                on:error={() => (errored = [...errored, post.id])}
+              />
+            {/if}
+          {/if}
+        </a>
       </div>
-    {/if}
-  {/each}
+    {/each}
+  </Masonry>
 
   {#if loading}
     <div class="flex w-full justify-center my-5">
